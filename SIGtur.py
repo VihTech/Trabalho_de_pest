cd4e4e1c7fe7f09fd9b1716434aca62b6b37b94d
VihTech <vitoriamaria.rodrigues02@gmail.com>
2023-06-02 20:27:14 -0300

    Terminanod a parte de alunos

--- SIGtur.py
+++ SIGtur.py
@@ -1,13 +1,11 @@
 from menus import*
 from funcoes import*
 
-#teste
-
 # Dicionarios
 
-dicionario_turma = dict()
-dicionario_professor = dict()
-dicionario_aluno = dict()
+dicionario_turma = pegar_dicionario('turmas')
+dicionario_professor = pegar_dicionario('professores')
+dicionario_aluno = pegar_dicionario('alunos')
 
 while True:
     escolha = menu_principal()
@@ -39,35 +37,54 @@ while True:
             if escolha_do_professor == '1':
                 while True:
                     nome = input('\n>>> Digite o nome do professor que deseja adicionar: ')
-                    matricula = int(input(f'>>> Digite a matricula do professor {nome}: '))
-                    if adicionar_atualizar(dicionario_professor, nome, matricula):
+                    matricula = input(f'>>> Digite a matricula do professor {nome}: ')
+                    retorno = adicionar_atualizar(dicionario_professor, nome, matricula)
+                    if retorno == True:
                         print('\n --- Cadastro realizado com sucesso ---')
+                        salvar_dicionarios(dicionario_professor, 'professores')
                         break
                     else:
-                        print('\n --- O nome deve ser composto e não pode conter números ---')
-
+                        print(retorno)
                 
             elif escolha_do_professor == '2':
                 while True:
-                    nome = input('\n>>> Digite o nome do professor que deseja adicionar: ')
-                    matricula = int(input(f'>>> Digite a matricula do professor {nome}: '))
-                    if adicionar_atualizar(dicionario_professor, nome, matricula):
-                        print('\n --- Cadastro realizado com sucesso ---')
-                        break
+
+                    visualizar(dicionario_professor)
+                    matricula = input('\n>>> Digite a matricula do professor que deseja editar: ')
+                    nome = input('>>> Digite novo nome do professor: ')
+
+                    if valor_existente(matricula, dicionario_professor):
+
+                        retorno = adicionar_atualizar(dicionario_professor, nome, matricula)
+                        if retorno:
+                            print('\n --- Atualização realizado com sucesso ---')
+                            salvar_dicionarios(dicionario_professor, 'professores')
+                            break
+                        else:
+                            print(retorno)
                     else:
-                        print('\n --- O nome deve ser composto e não pode conter números ---')
+                        print('\n--- Matricula Invalida ---\n')
 
             elif escolha_do_professor == '3':
-                pass
+                visualizar(dicionario_professor)
 
             elif escolha_do_professor == '4':
-                pass
+
+                while True:
+                    visualizar(dicionario_professor)
+                    matricula = input('\n>>> Digite a matricula do professor que deseja excluir: ')
+                    if valor_existente(matricula, dicionario_professor):
+                        excluir(dicionario_professor, matricula, 'professores')
+                        print('\n--- Professor excluido com sucesso ---')
+                        break
+                    else:
+                        print('\n--- Matricula Invalida ---\n')
 
             elif escolha_do_professor == '5':
                 pass
 
             elif escolha_do_professor == '6':
-                visualizar(dicionario_professor)
+                pass
 
             elif escolha_do_professor == '0':
                 break
@@ -80,24 +97,47 @@ while True:
             if escolha_do_aluno == '1':
                 while True:
                     nome = input('\n>>> Digite o nome do aluno que deseja adicionar: ')
-                    matricula = int(input(f'>>> Digite a matricula do aluno {nome}: '))
-                    if adicionar_atualizar(dicionario_aluno, nome, matricula):
+                    matricula = input(f'>>> Digite a matricula do aluno {nome}: ')
+                    retorno = adicionar_atualizar(dicionario_aluno, nome, matricula)
+                    if retorno == True:
                         print('\n --- Cadastro realizado com sucesso ---')
+                        salvar_dicionarios(dicionario_aluno, 'alunos')
                         break
                     else:
-                        print('\n --- O nome deve ser composto e não pode conter números ---')
+                        print(retorno)
                 
             elif escolha_do_aluno == '2':
-                pass
+                while True:
+
+                    visualizar(dicionario_aluno)
+                    matricula = input('\n>>> Digite a matricula do aluno que deseja editar: ')
+                    nome = input('>>> Digite o novo nome do aluno: ')
+
+                    if valor_existente(matricula, dicionario_aluno):
+
+                        retorno = adicionar_atualizar(dicionario_aluno, nome, matricula)
+                        if retorno:
+                            print('\n --- Atualização realizado com sucesso ---')
+                            salvar_dicionarios(dicionario_aluno, 'alunos')
+                            break
+                        else:
+                            print(retorno)
+                    else:
+                        print('\n--- Matricula Invalida ---\n')
 
             elif escolha_do_aluno == '3':
                 visualizar(dicionario_aluno)
 
             elif escolha_do_aluno == '4':
-                pass
-
-            elif escolha_do_aluno == '5':
-                pass
+                 while True:
+                    visualizar(dicionario_aluno)
+                    matricula = input('\n>>> Digite a matricula do aluno que deseja excluir: ')
+                    if valor_existente(matricula, dicionario_aluno):
+                        excluir(dicionario_aluno, matricula, 'alunos')
+                        print('\n--- Aluno excluido com sucesso ---')
+                        break
+                    else:
+                        print('\n--- Matricula Invalida ---\n')
 
             elif escolha_do_aluno == '0':
                 break
--- funcoes.py
+++ funcoes.py
@@ -1,15 +1,47 @@
+import json
+
 def adicionar_atualizar(dicionario, nome, matricula):
 
     if len(nome.split()) < 2 or nome.isnumeric():
-        return False
+        return '\n--- O nome deve ser composto e não pode conter números ---'
 
     else:  
-        dicionario[nome] = matricula
-        return True
+
+        if not matricula.isnumeric():
+            return '\n--- A matricula deve conter apenas números  ---'
+        else:
+            dicionario[matricula] = nome
+            return True
     
 def visualizar(dicionario):
-    print('=-' * 15 + '=')
-    print(f"{'Nome':^15} {'Matricula':>15}")
+    print('=-' * 18 + '=')
+    print(f"{'|Nome:':<20} {'|Matricula:':>16}")
     for itens, valores in dicionario.items():    
-        print(f'{itens:<15} {valores:^21}')
-    print('=-' * 15 + '=\n')
+        print('-'*37)
+        print(f'|{valores:<24} |{itens:>5}')
+    print('=-' * 18 + '=\n')
+
+def tratamento_repetido(dicionario, nome):
+    if nome not in dicionario:
+        return False
+    else:
+        return True
+
+def salvar_dicionarios(dicionario, nome_do_arquivo):
+    with open(f'{nome_do_arquivo}.json', 'w') as file:
+        json.dump(dicionario, file)
+
+def pegar_dicionario(nome_do_arquivo):
+    with open(f'{nome_do_arquivo}.json', 'r') as file:
+        return json.load(file)
+        
+def valor_existente(valor, dicionario):
+    if valor not in dicionario:
+        return False
+    else:
+        return True
+    
+def excluir(dicionario, matricula, nome_do_arquivo):
+    dicionario.pop(matricula)
+    salvar_dicionarios(dicionario, nome_do_arquivo)
+
